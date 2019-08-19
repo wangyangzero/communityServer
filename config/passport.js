325c23a02c6d08b61339a8652db1bdcd3efbb188
LazyCaty <1782445722@qq.com>
2019-08-19 10:16:10 +0800

    提交用户信息模型

---
+++ config/passport.js
@@ -0,0 +1,18 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+const mongoose = require('mongoose');
+const User = mongoose.model('userInfo');
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts,async (jwt_payload, done) => {
+        const user = await User.findById(jwt_payload.id);
+        if(user){
+            return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    }))
+}
---
+++ modals/user/userInfo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userInfoSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    nickname:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+    },
+    message:[
+        {
+            type:String,
+        }
+    ],
+});
+
+let userInfo = mongoose.model('userInfo',userInfoSchema);
+
+
+module.exports = userInfo;
